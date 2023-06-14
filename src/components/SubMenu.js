4a4310c5baed734a818a18f0119a6ab2e4476166
Hà Trung Tín <78654906+hatrungt1n@users.noreply.github.com>
2023-06-14 14:07:45 +0700

    fixbug: toggle submenu, need to specify id to toggle

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
+import React from "react";
 
-import {NavLink} from 'react-router-dom'; 
+import { NavLink } from "react-router-dom";
 
-
-const SubMenu = ({ item }) => {
-  const [subnav, setSubnav] = useState(false);
-
-  const showSubnav = () => setSubnav(!subnav);
-
+const SubMenu = ({ items, isOpen }) => {
   return (
     <>
-      <NavLink to={item.path} onClick={item.subNav && showSubnav}>
-        <div>
-          {item.icon}
-          <NavLink>{item.name}</NavLink>
-        </div>
-        <div>
-          {item.subNav && subnav
-            ? item.FaAngleDown
-            : item.subNav
-            ? item.FaAngleDown
-            : null }
-        </div>
-      </NavLink>
-      {subnav &&
-        item.subNav.map((item, index) => {
+      {isOpen &&
+        items.subNav?.map((item, index) => {
           return (
             <NavLink to={item.path} key={index}>
               {item.icon}
